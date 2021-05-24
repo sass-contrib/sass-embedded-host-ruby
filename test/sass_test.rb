d511c601019ebc5131b16fe70146a3dbc314643f
なつき <i@ntk.me>
2021-05-24 11:27:46 -0700

    Add test for top level Sass.render

---
+++ test/sass_test.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require_relative 'test_helper'
+
+module Sass
+  class SassTest < MiniTest::Test
+    def test_sass_works
+      assert_equal '', Sass.render({
+                                     data: ''
+                                   })[:css]
+
+      css = <<~CSS.chomp
+        h1 {
+          font-size: 2rem;
+        }
+      CSS
+      assert_equal css, Sass.render({
+                                      data: 'h1 { font-size: 2rem; }'
+                                    })[:css]
+    end
+  end
+end
