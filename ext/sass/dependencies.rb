5ab45a74a53eb82067c187f6e27e4759ffa86a39
なつき <i@ntk.me>
2021-09-15 23:33:33 -0700

    Update protobuf

--- ext/sass/dependencies.rb
+++ ext/sass/dependencies.rb
@@ -3,7 +3,7 @@
 module Sass
   module Dependencies
     REQUIREMENTS = {
-      'protocolbuffers/protobuf' => '~> 3.17',
+      'protocolbuffers/protobuf' => '~> 3.18.0',
       'sass/embedded-protocol' => '~> 1.0.0-beta.12',
       'sass/dart-sass-embedded' => '~> 1.0.0-beta.11'
     }.freeze
