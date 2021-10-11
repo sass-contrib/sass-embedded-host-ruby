b18d51e409807e4cb27ac23850ce9bb736376196
なつき <i@ntk.me>
2021-10-10 23:39:43 -0700

    Remove struct helper

--- lib/sass/embedded/result.rb
+++ lib/sass/embedded/result.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
-require_relative 'struct'
-
 module Sass
   class Embedded
     # The {RenderResult} of {Embedded#render}.
     class RenderResult
-      include Struct
-
       attr_reader :css, :map, :stats
 
       def initialize(css, map, stats)
@@ -19,8 +15,6 @@ module Sass
 
     # The {RenderResultStats} of {Embedded#render}.
     class RenderResultStats
-      include Struct
-
       attr_reader :entry, :start, :end, :duration
 
       def initialize(entry, start, finish, duration)
