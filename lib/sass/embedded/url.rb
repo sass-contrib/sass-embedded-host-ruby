09a1ab2c1907818a350087541d18e9dffeefd6e4
なつき <i@ntk.me>
2022-01-18 11:46:53 -0800

    Add FILE_SCHEME constant

--- lib/sass/embedded/url.rb
+++ lib/sass/embedded/url.rb
@@ -6,24 +6,26 @@ module Sass
   class Embedded
     # The {Url} module.
     module Url
-      # The {::URI::Parser} that is in consistent with Dart Uri.
+      # The {::URI::Parser} that is in consistent with RFC 2396 (URI Generic Syntax) and dart:core library.
       URI_PARSER = URI::Parser.new({ RESERVED: ';/?:@&=+$,' })
 
-      private_constant :URI_PARSER
+      FILE_SCHEME = 'file://'
+
+      private_constant :URI_PARSER, :FILE_SCHEME
 
       module_function
 
       def path_to_file_url(path)
         if File.absolute_path? path
-          "file://#{Platform::OS == 'windows' ? File::SEPARATOR : ''}#{URI_PARSER.escape(path)}"
+          URI_PARSER.escape "#{FILE_SCHEME}#{Platform::OS == 'windows' ? File::SEPARATOR : ''}#{path}"
         else
-          URI_PARSER.escape(path)
+          URI_PARSER.escape path
         end
       end
 
       def file_url_to_path(url)
-        if url.start_with? 'file://'
-          URI_PARSER.unescape url[(Platform::OS == 'windows' ? 8 : 7)..]
+        if url.start_with? FILE_SCHEME
+          URI_PARSER.unescape url[(Platform::OS == 'windows' ? (FILE_SCHEME.length + 1) : FILE_SCHEME.length)..]
         else
           URI_PARSER.unescape url
         end
