52e9504d2cf5cc47aeaa0de8747e8e281b507e36
なつき <i@ntk.me>
2022-02-24 08:31:02 -0800

    Fix legacy api
    - Closes #20

--- lib/sass/embedded/legacy.rb
+++ lib/sass/embedded/legacy.rb
@@ -372,29 +372,32 @@ module Sass
     # @deprecated
     # The {Url} module.
     module Url
-      # The {::URI::Parser} that is in consistent with RFC 2396 (URI Generic Syntax) and dart:core library.
-      URI_PARSER = URI::Parser.new({ RESERVED: ';/?:@&=+$,' })
-
       FILE_SCHEME = 'file://'
 
-      private_constant :URI_PARSER, :FILE_SCHEME
+      private_constant :FILE_SCHEME
 
       module_function
 
       def path_to_file_url(path)
         if File.absolute_path? path
-          URI_PARSER.escape "#{FILE_SCHEME}#{Gem.win_platform? ? '/' : ''}#{path}"
+          uri_parser.escape "#{FILE_SCHEME}#{Gem.win_platform? ? '/' : ''}#{path}"
         else
-          URI_PARSER.escape path
+          uri_parser.escape path
         end
       end
 
       def file_url_to_path(url)
         if url.start_with? FILE_SCHEME
-          URI_PARSER.unescape url[(FILE_SCHEME.length + (Gem.win_platform? ? 1 : 0))..]
+          uri_parser.unescape url[(FILE_SCHEME.length + (Gem.win_platform? ? 1 : 0))..]
         else
-          URI_PARSER.unescape url
+          uri_parser.unescape url
         end
+      end
+
+      private
+
+      def uri_parser
+        @uri_parser ||= URI::Parser.new({ RESERVED: ';/?:@&=+$,' })
       end
     end
 
