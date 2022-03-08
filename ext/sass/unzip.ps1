188ca00d34541f460ae4af19ad526ac853cbc0ad
なつき <i@ntk.me>
2022-03-08 10:25:38 -0800

    Update environment variable names for dependency overrides

--- ext/sass/unzip.ps1
+++ ext/sass/unzip.ps1
@@ -9,5 +9,5 @@ Param(
 if (Get-Command Expand-Archive -ErrorAction SilentlyContinue) {
     Get-Item $Archive | Expand-Archive -DestinationPath $DestinationPath -Force
 } else {
-    cscript unzip.vbs $Archive $DestinationPath
+    CScript unzip.vbs //B //Nologo $Archive $DestinationPath
 }
