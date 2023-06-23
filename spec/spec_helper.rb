63fdafe0d6b32943234b28b8cea80b0865840cfb
なつき <i@ntk.me>
2023-06-23 08:15:51 -0700

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,8 @@
 require 'sass-embedded'
 
 require 'json'
-require_relative './console'
-require_relative './sandbox'
+require_relative 'console'
+require_relative 'sandbox'
 
 RSpec.configure do |config|
   config.color_mode = :on if ENV.key?('GITHUB_ACTIONS')
