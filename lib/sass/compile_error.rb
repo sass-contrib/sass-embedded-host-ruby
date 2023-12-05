f22a525c344a63937d24ce327ee0184783c15f91
なつき <i@ntk.me>
2023-12-05 09:02:35 -0800

    Add CompileError#to_css_string

--- lib/sass/compile_error.rb
+++ lib/sass/compile_error.rb
@@ -15,6 +15,7 @@ module Sass
     # @!visibility private
     def initialize(message, full_message, sass_stack, span, loaded_urls)
       super(message)
+
       @full_message = full_message
       @sass_stack = sass_stack
       @span = span
@@ -23,9 +24,74 @@ module Sass
 
     # @return [String]
     def full_message(...)
-      return super(...) if @full_message.nil?
+      return super if @full_message.nil?
 
-      @full_message = +@full_message
+      +@full_message
+    end
+
+    # @return [String]
+    def to_css_string
+      message = full_message(highlight: false).gsub(/\e\[[0-9;]*m/, '')
+
+      <<~CSS
+        /* #{message.gsub('*/', "*\u2060/").gsub("\r\n", "\n").split("\n").join("\n * ")} */
+
+        body::before {
+          display: block;
+          padding: 1em;
+          margin-bottom: 1em;
+          border-bottom: 2px solid;
+          font-family: monospace, monospace;
+          white-space: pre;
+          content: #{serialize_quoted_string(message)};
+        }
+      CSS
+    end
+
+    private
+
+    def serialize_quoted_string(string, force_double_quote: false)
+      include_single_quote = false
+      include_double_quote = false
+
+      buffer = [0]
+      string.each_codepoint do |codepoint|
+        case codepoint
+        when 34
+          case
+          when force_double_quote
+            buffer << 92 << 34
+          when include_single_quote
+            return serialize_quoted_string(string, force_double_quote: true)
+          else
+            include_double_quote = true
+            buffer << 34
+          end
+        when 39
+          case
+          when force_double_quote
+            buffer << 39
+          when include_double_quote
+            return serialize_quoted_string(string, force_double_quote: true)
+          else
+            include_single_quote = true
+            buffer << 39
+          end
+        when 92
+          buffer << 92 << 92
+        else
+          if (codepoint < 32 && codepoint != 9) || codepoint > 126
+            buffer << 92
+            buffer.concat(codepoint.to_s(16).codepoints)
+            buffer << 32
+          else
+            buffer << codepoint
+          end
+        end
+      end
+      buffer[0] = force_double_quote || !include_double_quote ? 34 : 39
+      buffer << buffer[0]
+      buffer.pack('U*')
     end
   end
 end
