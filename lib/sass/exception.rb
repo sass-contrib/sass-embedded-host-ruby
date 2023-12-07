99a70e98c761a52c8eaf32006279b119a11b44ea
なつき <i@ntk.me>
2023-12-07 13:10:46 -0800

    Use inline rubocop:disable

--- lib/sass/exception.rb
+++ lib/sass/exception.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 unless Exception.respond_to?(:to_tty?)
-  # rubocop:disable Style/GlobalStdStream
   def Exception.to_tty?
-    $stderr == STDERR && STDERR.tty?
+    $stderr == STDERR && STDERR.tty? # rubocop:disable Style/GlobalStdStream
   end
-  # rubocop:enable Style/GlobalStdStream
 end
