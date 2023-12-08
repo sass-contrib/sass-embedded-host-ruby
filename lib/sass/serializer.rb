c14a2b215913c634a90163ab96b2a3703e425260
なつき <i@ntk.me>
2023-12-07 23:37:10 -0800

    Optimize serializer

--- lib/sass/serializer.rb
+++ lib/sass/serializer.rb
@@ -5,7 +5,7 @@ module Sass
   module Serializer
     module_function
 
-    def dump_quoted_string(string, force_double_quote: false)
+    def dump_quoted_string(string)
       include_single_quote = false
       include_double_quote = false
 
@@ -13,25 +13,15 @@ module Sass
       string.each_codepoint do |codepoint|
         case codepoint
         when 34
-          case
-          when force_double_quote
-            buffer << 92 << 34
-          when include_single_quote
-            return dump_quoted_string(string, force_double_quote: true)
-          else
-            include_double_quote = true
-            buffer << 34
-          end
+          return dump_double_quoted_string(string) if include_single_quote
+
+          include_double_quote = true
+          buffer << 34
         when 39
-          case
-          when force_double_quote
-            buffer << 39
-          when include_double_quote
-            return dump_quoted_string(string, force_double_quote: true)
-          else
-            include_single_quote = true
-            buffer << 39
-          end
+          return dump_double_quoted_string(string) if include_double_quote
+
+          include_single_quote = true
+          buffer << 39
         when 92
           buffer << 92 << 92
         when 9
@@ -46,8 +36,36 @@ module Sass
           end
         end
       end
-      buffer[0] = 39 unless force_double_quote || !include_double_quote
-      buffer << buffer[0]
+      if include_double_quote
+        buffer[0] = 39
+        buffer << 39
+      else
+        buffer << 34
+      end
+      buffer.pack('U*')
+    end
+
+    def dump_double_quoted_string(string)
+      buffer = [34]
+      string.each_codepoint do |codepoint|
+        case codepoint
+        when 34
+          buffer << 92 << 34
+        when 92
+          buffer << 92 << 92
+        when 9
+          buffer << 9
+        else
+          if codepoint < 32 || codepoint > 126
+            buffer << 92
+            buffer.concat(codepoint.to_s(16).codepoints)
+            buffer << 32
+          else
+            buffer << codepoint
+          end
+        end
+      end
+      buffer << 34
       buffer.pack('U*')
     end
   end
