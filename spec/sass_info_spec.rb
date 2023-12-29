06c8df3cf6c55ed75955d4bda23868bef838c849
なつき <i@ntk.me>
2023-12-29 12:50:26 -0800

    Add spec for Sass#info

---
+++ spec/sass_info_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Sass do
+  describe '.info' do
+    it 'begins with a unique identifier for the Sass implementation' do
+      expect(described_class.info).to start_with("sass-embedded\t")
+    end
+
+    it 'followed by its package version' do
+      version = described_class.info.split("\t")[1]
+      expect { Gem::Version.new(version) }.not_to raise_error
+    end
+  end
+end
