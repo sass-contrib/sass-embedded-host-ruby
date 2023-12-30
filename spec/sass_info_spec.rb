4f9b264ca1178074c0d2923c5705b240c78ca56c
なつき <i@ntk.me>
2023-12-29 19:03:59 -0800

    Update sass_info_spec.rb

--- spec/sass_info_spec.rb
+++ spec/sass_info_spec.rb
@@ -4,12 +4,18 @@ require 'spec_helper'
 
 RSpec.describe Sass do
   describe '.info' do
+    identifier = 'sass-embedded'
+
     it 'begins with a unique identifier for the Sass implementation' do
-      expect(described_class.info).to start_with("sass-embedded\t")
+      expect(described_class.info).to start_with(identifier)
+    end
+
+    it 'followed by U+0009 TAB' do
+      expect(described_class.info[identifier.length]).to eq("\t")
     end
 
     it 'followed by its package version' do
-      version = described_class.info.split[1]
+      version = described_class.info.slice(identifier.length + 1).split[0]
       expect { Gem::Version.new(version) }.not_to raise_error
     end
   end
