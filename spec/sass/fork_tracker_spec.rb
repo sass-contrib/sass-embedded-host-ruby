7d3de983483ab8d9384604780c75e1c9f7bf563e
なつき <i@ntk.me>
2023-12-30 10:17:42 -0800

    Add spec for Sass::ForkTracker

--- spec/sass/fork_tracker_spec.rb
+++ spec/sass/fork_tracker_spec.rb
@@ -3,8 +3,55 @@
 require 'spec_helper'
 
 RSpec.describe 'Sass::ForkTracker', skip: (Process.respond_to?(:fork) ? false : 'Process.fork is not usable') do
-  describe 'global compiler after fork' do
-    it 'works in parent process' do
+  let(:described_class) do
+    Sass.const_get(:ForkTracker)
+  end
+
+  it 'tracks global compiler process' do
+    Sass.compile_string('a { b: c }')
+
+    expect(described_class.each.size).to eq(1)
+  end
+
+  it 'tracks new compiler process' do
+    compiler = Sass::Compiler.new
+
+    expect(described_class.each.size).to eq(2)
+
+    compiler.close
+
+    expect(described_class.each.size).to eq(1)
+  end
+
+  it 'tracks running compiler processes in parent process after fork' do
+    expect(described_class.each.size).to eq(1)
+
+    pid = Process.fork do
+      exit 0
+    end
+    _, result = Process.wait2(pid)
+    expect(result.exitstatus).to be(0)
+
+    expect(described_class.each.size).to eq(1)
+  end
+
+  it 'closes running compiler processes in child process after fork' do
+    compiler = Sass::Compiler.new
+
+    pid = Process.fork do
+      expect(described_class.each.size).to eq(0)
+      exit 0
+    rescue StandardError
+      exit 1
+    end
+    _, result = Process.wait2(pid)
+    expect(result.exitstatus).to be(0)
+
+    compiler.close
+  end
+
+  describe 'global compiler' do
+    it 'works in parent process after fork' do
       pid = Process.fork do
         exit 0
       end
@@ -15,7 +62,7 @@ RSpec.describe 'Sass::ForkTracker', skip: (Process.respond_to?(:fork) ? false : 
         .to eq("a {\n  b: c;\n}")
     end
 
-    it 'works in child process' do
+    it 'works in child process after fork' do
       pid = Process.fork do
         expect(Sass.compile_string('a {b: c}').css)
           .to eq("a {\n  b: c;\n}")
@@ -27,7 +74,7 @@ RSpec.describe 'Sass::ForkTracker', skip: (Process.respond_to?(:fork) ? false : 
       expect(result.exitstatus).to be(0)
     end
 
-    it 'works in both parent and child process' do
+    it 'works in both parent and child process after fork' do
       pid = Process.fork do
         expect(Sass.compile_string('a {b: c}').css)
           .to eq("a {\n  b: c;\n}")
@@ -43,8 +90,8 @@ RSpec.describe 'Sass::ForkTracker', skip: (Process.respond_to?(:fork) ? false : 
     end
   end
 
-  describe 'opened compiler after fork' do
-    it 'works in parent process' do
+  describe 'new compiler' do
+    it 'works in parent process after fork' do
       sass = Sass::Compiler.new
 
       pid = Process.fork do
@@ -59,7 +106,7 @@ RSpec.describe 'Sass::ForkTracker', skip: (Process.respond_to?(:fork) ? false : 
       sass&.close
     end
 
-    it 'works in child process' do
+    it 'works in child process after fork' do
       sass = Sass::Compiler.new
 
       pid = Process.fork do
@@ -77,7 +124,7 @@ RSpec.describe 'Sass::ForkTracker', skip: (Process.respond_to?(:fork) ? false : 
       sass&.close
     end
 
-    it 'works in both parent and child process' do
+    it 'works in both parent and child process after fork' do
       sass = Sass::Compiler.new
 
       pid = Process.fork do
@@ -99,8 +146,8 @@ RSpec.describe 'Sass::ForkTracker', skip: (Process.respond_to?(:fork) ? false : 
     end
   end
 
-  describe 'closed compiler after fork' do
-    it 'remains closed in parent process' do
+  describe 'closed compiler' do
+    it 'remains closed in parent process after fork' do
       sass = Sass::Compiler.new
       sass.close
       expect(sass.closed?).to be(true)
@@ -116,7 +163,7 @@ RSpec.describe 'Sass::ForkTracker', skip: (Process.respond_to?(:fork) ? false : 
       sass&.close
     end
 
-    it 'remains closed in child process' do
+    it 'remains closed in child process after fork' do
       sass = Sass::Compiler.new
       sass.close
       expect(sass.closed?).to be(true)
