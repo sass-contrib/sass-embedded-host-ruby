0b982e28565ae10c88acc2f721ca53b76999f844
なつき <i@ntk.me>
2024-02-07 08:15:15 -0800

    Update node_package_importer.rb

--- lib/sass/node_package_importer.rb
+++ lib/sass/node_package_importer.rb
@@ -7,6 +7,8 @@ module Sass
   # @see https://sass-lang.com/documentation/js-api/classes/nodepackageimporter/
   class NodePackageImporter
     def initialize(entry_point_directory)
+      raise ArgumentError, 'entry_point_directory must be set' if entry_point_directory.nil?
+
       @entry_point_directory = File.absolute_path(entry_point_directory)
     end
   end
