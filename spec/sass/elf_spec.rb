05f304466af2b5fd0308e7f3079c06907e592023
なつき <i@ntk.me>
2025-01-26 11:40:12 -0800

    Support Linuxulator on FreeBSD

--- spec/sass/elf_spec.rb
+++ spec/sass/elf_spec.rb
@@ -7,32 +7,35 @@ RSpec.describe 'Sass::ELF', skip: (Sass.const_defined?(:ELF) ? false : 'Sass::EL
     Sass.const_get(:ELF)
   end
 
-  describe 'ruby program interpreter' do
-    it 'starts with ld-' do
+  describe 'ruby', skip: (File.exist?('/proc/self/exe') ? false : 'procfs is not available') do
+    it 'extracts program interpreter' do
       expect(File.basename(described_class::INTERPRETER)).to start_with('ld-')
+    end
+
+    it 'dumps elf headers' do
+      input = StringIO.new(File.binread('/proc/self/exe'))
+      output = StringIO.new.binmode
+
+      described_class.new(input).dump(output)
+      expect(output.string).to eq(input.string.slice(0, output.length))
     end
   end
 
-  describe 'dart program interpreter' do
+  describe 'dart' do
     subject(:interpreter) do
       Sass.const_get(:CLI)::INTERPRETER
     end
 
-    it 'starts with ld-' do
+    it 'extracts program interpreter' do
       expect(File.basename(interpreter)).to start_with('ld-')
     end
 
-    it 'is the same as ruby' do
-      expect(File.basename(interpreter))
-        .to eq(File.basename(described_class::INTERPRETER))
+    it 'dumps elf headers' do
+      input = StringIO.new(File.binread(Sass.const_get(:CLI)::COMMAND[0]))
+      output = StringIO.new.binmode
+
+      described_class.new(input).dump(output)
+      expect(output.string).to eq(input.string.slice(0, output.length))
     end
-  end
-
-  it 'dumps elf headers' do
-    input = StringIO.new(File.binread('/proc/self/exe'))
-    output = StringIO.new.binmode
-
-    described_class.new(input).dump(output)
-    expect(output.string).to eq(input.string.slice(0, output.length))
   end
 end
