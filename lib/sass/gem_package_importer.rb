d59870aeecc9d6e61519ad683a85bb444c2b52bb
なつき <i@ntk.me>
2025-10-27 11:21:47 -0700

    Test Sass::GemPackageImporter with path containing special characters (#342)

--- lib/sass/gem_package_importer.rb
+++ lib/sass/gem_package_importer.rb
@@ -7,11 +7,11 @@ module Sass
     def find_file_url(url, _canonicalize_context)
       return unless url.start_with?('pkg:')
 
-      library, _, path = url[4..].partition(/[?#]/).first.partition('/')
+      library, _, path = url[4..].partition(/[#?]/).first.partition('/')
       gem_dir = Gem::Dependency.new(library).to_spec.gem_dir
       gem_dir = "/#{gem_dir}" unless gem_dir.start_with?('/')
 
-      "file://#{gem_dir.gsub(/[?#%]/, { '?' => '%3F', '#' => '%23', '%' => '%25' })}/#{path}"
+      "file://#{gem_dir.gsub(/[#%?\\]/, { '#' => '%23', '%' => '%25', '?' => '%3F', '\\' => '%5C' })}/#{path}"
     rescue Gem::MissingSpecError
       nil
     end
