26f472cd40c60e8745978f9938078996b62a1fef
なつき <i@ntk.me>
2026-01-11 09:07:14 -0800

    Avoid an extra mutex on platforms without fork support

--- lib/sass/fork_tracker.rb
+++ lib/sass/fork_tracker.rb
@@ -5,46 +5,54 @@ module Sass
   #
   # It tracks objects that need to be closed after `Process.fork`.
   module ForkTracker
-    HASH = {}.compare_by_identity
-
-    MUTEX = Mutex.new
-
-    private_constant :HASH, :MUTEX
-
     module_function
 
-    def add(obj)
-      MUTEX.synchronize do
-        HASH[obj] = true
+    if Process.respond_to?(:_fork)
+      # TODO: remove next line once ruby 3.1 support is dropped
+      require 'set' unless defined?(::Set)
+
+      SET = Set.new.compare_by_identity
+
+      MUTEX = Mutex.new
+
+      private_constant :SET, :MUTEX
+
+      def add(object)
+        MUTEX.synchronize do
+          SET.add(object)
+        end
       end
-    end
 
-    def delete(obj)
-      MUTEX.synchronize do
-        HASH.delete(obj)
+      def delete(object)
+        MUTEX.synchronize do
+          SET.delete(object)
+        end
       end
-    end
 
-    def each(...)
-      MUTEX.synchronize do
-        HASH.keys
-      end.each(...)
-    end
-
-    # The {CoreExt} module.
-    #
-    # It closes objects after `Process.fork`.
-    module CoreExt
-      def _fork
-        pid = super
-        ForkTracker.each(&:close) if pid.zero?
-        pid
+      def each(&)
+        MUTEX.synchronize do
+          SET.to_a
+        end.each(&)
       end
+
+      # The {CoreExt} module.
+      #
+      # It closes objects after `Process.fork`.
+      module CoreExt
+        def _fork
+          pid = super
+          ForkTracker.each(&:close) if pid.zero?
+          pid
+        end
+      end
+
+      private_constant :CoreExt
+
+      Process.singleton_class.prepend(CoreExt)
+    else
+      def add(object); end
+      def delete(object); end
     end
-
-    private_constant :CoreExt
-
-    Process.singleton_class.prepend(CoreExt) if Process.respond_to?(:_fork)
   end
 
   private_constant :ForkTracker
